3a1972892a88d13a12527d9f28c28445891bb820
anthonysjce <anthonysjce@gmail.com>
2025-12-29 19:14:18 +0530

    commit2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,27 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RecipeModel } from './models';
+import { MOCK_RECIPES } from './mock-recipes';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('tutor');
+  protected readonly recipie = signal<RecipeModel>(MOCK_RECIPES[0]);
+  protected readonly count = signal(0);
+  protected handleButton1Click() {
+    this.recipie.set(MOCK_RECIPES[0]);
+  }
+  protected handleButton2Click() {
+    this.recipie.set(MOCK_RECIPES[1]);
+  }
+  protected increment() {
+    this.count.update((currentCount) => currentCount + 1);
+  }
+  protected decrement() {
+    this.count.update((currentCount) => currentCount - 1);
+  }
 }
