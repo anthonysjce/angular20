089eabe959ad08bf01843f0df3a5148489fc102b
anthonysjce <anthonysjce@gmail.com>
2026-04-21 19:10:34 +0530

    before recipe list

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,27 +1,37 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RecipeModel } from './models';
 import { MOCK_RECIPES } from './mock-recipes';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [JsonPipe],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
-  protected readonly recipie = signal<RecipeModel>(MOCK_RECIPES[0]);
-  protected readonly count = signal(0);
-  protected handleButton1Click() {
-    this.recipie.set(MOCK_RECIPES[0]);
+  protected readonly recipe = signal<RecipeModel>(MOCK_RECIPES[0]);
+  protected readonly servings = signal(2);
+  //protected readonly adjustedIngredients =
+  protected readonly adjustedIngredients = computed(() => {
+    return this.recipe().ingredients.map((item) => {
+      return { ...item, quantity: item.quantity * this.servings() };
+    });
+  });
+  protected handleButton1Click(): void {
+    this.recipe.set(MOCK_RECIPES[0]);
   }
-  protected handleButton2Click() {
-    this.recipie.set(MOCK_RECIPES[1]);
+
+  protected handleButton2Click(): void {
+    this.recipe.set(MOCK_RECIPES[1]);
   }
-  protected increment() {
-    this.count.update((currentCount) => currentCount + 1);
+
+  protected increment(): void {
+    this.servings.update((currentServings) => currentServings + 1);
   }
-  protected decrement() {
-    this.count.update((currentCount) => currentCount - 1);
+
+  protected decrement(): void {
+    this.servings.update((currentServings) => Math.max(1, currentServings - 1));
   }
 }
